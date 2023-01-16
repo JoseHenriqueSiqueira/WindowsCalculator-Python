465ce6cbb5caef25c9dd05ae8801cc8321f96c96
José Henrique Siqueira <116378234+JoseHenriqueSiqueira@users.noreply.github.com>
2023-01-16 20:59:10 -0300

    Update

--- Calculator.py
+++ Calculator.py
@@ -10,10 +10,10 @@ class Window(QWidget):
         self.setFixedSize(320, 470)
         loadUi(r"Ui/Interface.ui",self)
         self.operations = {
-            "+": lambda: int(self.nm1) + int(self.nm2),
-            "-": lambda: abs(int(self.nm1)) - abs(int(self.nm2)) if self.nm2!='0' else (abs(int(self.nm1)) - abs(int(self.nm2)))*-1,
-            "x": lambda: int(self.nm1) * int(self.nm2),
-            "÷": lambda: float(self.nm1) // float(self.nm2) if divmod(float(self.nm1), float(self.nm2))[1] == 0 else float(self.nm1) / float(self.nm2)
+            "+": lambda: float(self.nm1) + float(self.nm2),
+            "-": lambda: abs(float(self.nm1)) - abs(float(self.nm2)) if self.nm2!='0' else (abs(float(self.nm1)) - abs(float(self.nm2)))*-1,
+            "x": lambda: float(self.nm1) * float(self.nm2),
+            "÷": lambda: float(self.nm1) / float(self.nm2),
         }
         self.op=None
         self.nm1=None
@@ -32,38 +32,51 @@ class Window(QWidget):
         self.btnmult.clicked.connect(lambda: self.operation("x"))
         self.btndiv.clicked.connect(lambda: self.operation("÷"))
         self.btnresult.clicked.connect(self.result)
+        self.btndecimal.setText(QLocale().decimalPoint())
+        self.btndecimal.clicked.connect(self.number)
 
     def number(self):
         number = self.sender().text()
         text=self.lineEdit.text()
+        decimal=QLocale().decimalPoint()
+        if number == decimal and text.count(decimal) > 0:
+            return
         if self.op:
-            if self.temp==0:
+            if self.temp==0 and number!=decimal:
                 self.lineEdit.setText(number)
-                self.temp=1
             else:
                 self.lineEdit.setText(text+number)
+            self.temp=1
             self.nm2=self.lineEdit.text()
             return
         else:
-            if self.temp==0:
+            if self.temp==0 and number!=decimal:
                 self.lineEdit.setText(number)
-                self.temp=1
             else:
                 self.lineEdit.setText(text+number)
+            self.temp=1
             self.nm1=self.lineEdit.text()
             return
 
     def operation(self, op):
         if self.op and self.nm2 and self.nm1:
             try:
-                self.nm1=str(self.operations.get(self.op)())
+                self.nm1 = QLocale().toFloat(self.nm1)[0]
+                self.nm2 = QLocale().toFloat(self.nm2)[0]
+                result=self.operations.get(self.op)()
+                formatted_result=QLocale().toString(result, 'f', 2)
+                if ',' in formatted_result:
+                    parts = formatted_result.split(',')
+                    if parts[1] and all(c == '0' for c in parts[1]):
+                        formatted_result = parts[0] # remove trailing zeros   
+                self.nm1=formatted_result
                 self.nm2=None
                 self.lineEdit.setText(self.nm1)
                 self.temp=0
                 self.label.setText(self.nm1+" "+ op)
                 self.op=op
             except ZeroDivisionError:
-                self.lineEdit.setText("Não é possível dividir por zero")
+                self.lineEdit.setText("Zero Division")
         elif self.nm1 and self.negative=='s':
             self.negative=""
             self.nm1=str(int(self.nm1)*-1)
@@ -87,15 +100,24 @@ class Window(QWidget):
     def result(self):
         if self.op in self.operations and self.nm2:
             try:
-                result = self.operations[self.op]()
-                self.lineEdit.setText(str(result))
-                self.label.setText(f"{self.nm1} {self.op} {self.nm2} =")
-                self.nm1 = str(result)
+                textn1=self.nm1
+                textn2=self.nm2
+                self.nm1 = QLocale().toFloat(self.nm1)[0]
+                self.nm2 = QLocale().toFloat(self.nm2)[0]
+                result=self.operations.get(self.op)()
+                formatted_result=QLocale().toString(result, 'f', 2)
+                if ',' in formatted_result:
+                    parts = formatted_result.split(',')
+                    if parts[1] and all(c == '0' for c in parts[1]):
+                        formatted_result = parts[0] # remove trailing zeros    
+                self.lineEdit.setText(formatted_result)
+                self.label.setText(f"{textn1} {self.op} {textn2} =")
+                self.nm1 = formatted_result
                 self.nm2 = None
                 self.temp = 0
                 self.op = ""
             except ZeroDivisionError:
-                self.lineEdit.setText("Não é possível dividir por zero")
+                self.lineEdit.setText("Zero Division")
 
     def reset(self):
         self.op=""
@@ -149,7 +171,7 @@ class Window(QWidget):
         
     def keyPressEvent(self, event):
         numbers_key = [Qt.Key_0, Qt.Key_1, Qt.Key_2, Qt.Key_3, Qt.Key_4, Qt.Key_5, Qt.Key_6, Qt.Key_7, Qt.Key_8, Qt.Key_9]
-        operations_key = {Qt.Key_Minus: "sub", Qt.Key_Plus:"sum", Qt.Key_Asterisk:"mult", Qt.Key_Slash:"div", Qt.Key_Equal:"result", Qt.Key_Enter:"result", Qt.Key_Return:"result", Qt.Key_Backspace:"del", Qt.Key_Delete:"del", 44:'del'}
+        operations_key = {Qt.Key_Minus: "sub", Qt.Key_Plus:"sum", Qt.Key_Asterisk:"mult", Qt.Key_Slash:"div", Qt.Key_Equal:"result", Qt.Key_Enter:"result", Qt.Key_Return:"result", Qt.Key_Backspace:"del", Qt.Key_Delete:"del", 44:'decimal'}
         if event.key() in numbers_key:
             n = event.key() - Qt.Key_0 #convert the key code to the corresponding number
             button = getattr(self, f"btn{n}")
